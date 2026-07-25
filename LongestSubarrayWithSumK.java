8adef4c4ed4a6d3c27bb22ee18c22cb100b4899b
Sabari-D <sabaridhandapani69@gmail.com>
2026-07-25 06:06:50 +0530

    LongestSubarrayWithSumK.java

--- LongestSubarrayWithSumK.java
+++ LongestSubarrayWithSumK.java
@@ -27,7 +27,7 @@ public class Main
 	}
 }
 
-//********************************************************* BRUTE FORCE 2 ***********************************************************
+//********************************************************* BRUTE FORCE 2 (POSITIVES ONLY )***********************************************************
 import java.util.*;
 public class Main
 {
@@ -54,6 +54,84 @@ public class Main
 		 }
 		System.out.print(maxLen);
 		
+	}
+}
+
+
+// ************************************************************* BETTER SOL (FOR ZEROES INCLUDING) ******************************************
+// SOLVES FOR BOTH POSITIVES AND NEGATIVES AND ZEROES
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] arr =  Arrays.stream(scanner.nextLine().split(" "))
+		            .mapToInt(Integer :: parseInt)
+		            .toArray();
+		            
+		int k = scanner.nextInt();
+		
+	    Map<Long, Integer> prefixSum = new HashMap<>();
+	    long sum = 0;
+	    int maxLen = 0;
+	    
+	    for(int i=0; i<arr.length; i++){
+	        sum += arr[i];
+	        
+	        if(sum == k){
+	            maxLen = Math.max(maxLen, i+1);
+	        }
+	        
+	        long rem = sum - k;
+	        if(prefixSum.containsKey(rem)){
+	            int len = i-prefixSum.get(rem);
+	            maxLen = Math.max(maxLen, len);
+	        }
+	        
+	        if(!prefixSum.containsKey(sum)){
+	            prefixSum.put(sum, i);
+	        }
+	    }
+	    
+	    System.out.print(maxLen);
+		
+	}
+}
+
+//******************************************************* OPTIMAL SOL *********************************************************************************
+// POSITVES AND ZEROES
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] arr =  Arrays.stream(scanner.nextLine().split(" "))
+		            .mapToInt(Integer :: parseInt)
+		            .toArray();
+		            
+		int k = scanner.nextInt();
+		
+	    int left = 0, right = 0;
+	    int maxLen = 0;
+	    int size = arr.length;
+	    int sum = arr[0];
+	    while(right < size){
+	        while(left <= right && sum > k){
+	            sum -= arr[left];
+	            left++;
+	        }
+	        
+	        if(sum == k){
+	            maxLen = Math.max(maxLen, right-left+1);
+	        }
+	       
+	        right++;
+	        if(right < size) sum += arr[right];
+	        
+	    }
+		System.out.print(maxLen);
 	}
 }
 
