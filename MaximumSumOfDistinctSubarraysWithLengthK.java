aaefcc0cfe31956d419b8eceb23620bab2d33a11
Sabari-D <sabaridhandapani69@gmail.com>
2026-08-01 23:47:25 +0530

    Maximum sum of distinct subarrays of length k

---
+++ MaximumSumOfDistinctSubarraysWithLengthK.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int left = 0;
+        long currentSum = 0;
+        long maxSum = 0;
+
+
+        Set<Integer> set = new HashSet<>();
+        for(int right=0; right < nums.length; right++){
+            if(!set.contains(nums[right])){
+                currentSum += nums[right];
+                set.add(nums[right]);
+
+                if(right-left+1 == k){
+                    maxSum = Math.max(maxSum, currentSum);
+                    currentSum -= nums[left];
+                    set.remove(nums[left]);
+                    left++;
+                }
+            }else{
+                while(nums[left] != nums[right]){
+                    currentSum -= nums[left];
+                    set.remove(nums[left]);
+                    left++;
+                }
+                left++;
+            }
+        }
+    return maxSum;
+        
+    }
+}
