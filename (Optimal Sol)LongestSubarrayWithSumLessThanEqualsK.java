8a1de6a2e655e36306d53d5d9e0e0d5bb2773f5f
Sabari-D <sabaridhandapani69@gmail.com>
2026-08-05 22:11:01 +0530

    Implement solution for longest subarray with sum <= K

---
+++ (Optimal Sol)LongestSubarrayWithSumLessThanEqualsK.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] arr = Arrays.stream(scanner.nextLine().split(" "))
+		            .mapToInt(Integer :: parseInt)
+		            .toArray();
+		
+		int k = scanner.nextInt();
+		
+		int left = 0, right= 0, sum = 0, maxSum = 0;
+		
+		while(right < arr.length){
+		    sum += arr[right];
+		    if(sum > k && left <= right){
+		        sum -= arr[right];
+		        left++;
+		    }
+		    if(sum <= k){
+		        maxSum = Math.max(maxSum, right-left+1);
+		    }
+		    right++;
+		}
+		System.out.print(maxSum);
+	}
+}
