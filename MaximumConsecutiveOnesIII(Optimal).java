f67b7203388897df20ecb8a1bd44314b6ab9e64a
Sabari-D <sabaridhandapani69@gmail.com>
2026-08-10 06:31:58 +0530

    Implement solution for longest consecutive ones with flips

---
+++ MaximumConsecutiveOnesIII(Optimal).java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0, right = 0, maxLen = 0, zeroes = 0;
+
+        while(right < nums.length){
+            if(nums[right] == 0) zeroes++;
+
+            while(zeroes > k){
+                if(nums[left] == 0) zeroes--;
+                left++;
+            }
+
+            if(zeroes <= k){
+                int len = right-left+1;
+                maxLen = Math.max(len, maxLen);
+            }
+            right++;
+        }
+    return maxLen;
+        
+    }
+}
