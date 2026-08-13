7031f0bb684cb3f3e0e2347853d8f951f4191e8b
Sabari-D <sabaridhandapani69@gmail.com>
2026-08-13 20:29:33 +0530

    Implement array rotation by one using reverse

---
+++ RotateArrayByOne.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void rotate(int[] arr) {
+        // code here
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, 1, arr.length-1);
+    }
+    public static void reverse(int[] arr, int start, int end){
+        while(start <=  end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
